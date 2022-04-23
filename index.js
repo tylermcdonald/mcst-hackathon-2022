cc9100841f9d83048df612d4b631dee2fda07096
Tyler McDonald <tyler.mcdonald545@gmail.com>
2022-04-23 12:01:12 -0400

    Step 2: Logging existing users in

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js'
-import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -13,16 +13,49 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
+const getEmailAndPassword = (prefix) => {
+    const email = document.getElementById(`${prefix}-email`).value;
+    const password = document.getElementById(`${prefix}-password`).value;
+    if (!email || email.trim().length === 0 || !password || email.trim().length === 0) {
+        return {};
+    }
+    return { email, password };
+}
+
+const setFormError = (message) => {
+    // TODO Set error on form
+};
 
 function createUserAccount(e) {
     e.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    if(!email || email.trim().length === 0 || !password || email.trim().length === 0) {
-        // TODO Set error on form
+    const { email, password } = getEmailAndPassword('signup');
+    if (!email || !password) {
+        setFormError('Invalid email or password');
         return;
     }
     createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            console.log(user);
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            setFormError(errorMessage);
+        });
+}
+
+function loginUser(e) {
+    e.preventDefault();
+    const { email, password } = getEmailAndPassword('login');
+    if (!email || !password) {
+        setFormError('Invalid email or password');
+        return;
+    }
+
+    signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             // Signed in 
             const user = userCredential.user;
@@ -31,9 +64,9 @@ function createUserAccount(e) {
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
-            // TODO Set error on form
-            // ..
+            setFormError(errorMessage);
         });
 }
 
 document.getElementById('create-account-button').onclick = createUserAccount;
+document.getElementById('login-button').onclick = loginUser;
